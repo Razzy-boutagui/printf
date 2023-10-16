4d33b34beb20482db690174eded605af5742e28f
Razzy-boutagui <abderrazzak.boutagui@gmail.com>
2023-10-16 09:18:18 -0700

    ss

--- _printf.c
+++ _printf.c
@@ -1,48 +1,73 @@
-#include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
 
 /**
- * _printf - a function that selects the correct function to print based on format.
- * @format: a format string.
- * Return: the length of the printed string.
+ * _printf - Custom printf function.
+ * @format: A format string containing conversion specifiers.
+ * @...: Variable number of arguments for the conversion specifiers.
+ *
+ * Return: The number of characters printed (excluding the null byte).
  */
 int _printf(const char *format, ...)
 {
-    convert_match m[] = {
-        {"%s", printf_string}, {"%c", printf_char},
-        {"%%", printf_percent},
-        {"%i", printf_int}, {"%d", printf_decimal}, {"%r", printf_reverse},
-        {"%R", printf_rot13}, {"%b", printf_binary}, {"%u", printf_unsigned},
-        {"%o", printf_octal}, {"%x", printf_hex}, {"%X", printf_hex_upper},
-        {"%S", printf_special_string}, {"%p", printf_pointer}
-    };
-
+    int char_count = 0;
     va_list args;
-    int i = 0, j, len = 0;
 
     va_start(args, format);
-    if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-        return (-1);
 
-    while (format[i] != '\0')
+    while (format && format[char_count])
     {
-        j = 13;
-        while (j >= 0)
+        if (format[char_count] == '%')
         {
-            if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
+            char_count++; // Move past '%'
+
+            if (format[char_count] == 'c')
             {
-                len += m[j].f(args);
-                i = i + 2;
-                goto end_of_loop;
+                // Handle %c (character specifier)
+                char c = va_arg(args, int);
+                write(1, &c, 1); // Write the character
+                char_count++; // Move past 'c'
             }
-            j--;
+            else if (format[char_count] == 's')
+            {
+                // Handle %s (string specifier)
+                char *s = va_arg(args, char *);
+                int len = 0;
+                while (s && s[len])
+                {
+                    len++;
+                }
+                write(1, s, len); // Write the string
+                char_count++; // Move past 's'
+            }
+            else if (format[char_count] == '%')
+            {
+                // Handle %%
+                write(1, "%", 1); // Write a single %
+                char_count++; // Move past the second '%'
+            }
+            // Add additional cases for other conversion specifiers here
         }
-        _putchar(format[i]);
-        len++;
-        i++;
+        else
+        {
+            // Handle regular characters, not conversion specifiers
+            write(1, &format[char_count], 1); // Write the character
+            char_count++;
+        }
     }
 
-end_of_loop:
     va_end(args);
-    return (len);
+
+    return char_count;
 }
+
+int main(void)
+{
+    int len;
+
+    len = _printf("Let's try to printf a simple sentence.\n");
+    _printf("Length:[%d]\n", len);
+    len = _printf("Character:[%c]\n", 'H');
+    _printf("Length:[%d]\n", len);
+   
 
